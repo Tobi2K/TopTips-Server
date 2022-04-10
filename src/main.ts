d5bb201887b4add9c85e2abae9fd94265dee41a7
Tobias Kalmbach <t.kalmbach35@gmail.com>
2022-04-10 10:19:46 +0200

    CI Test

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  console.log('Test');
+
   await app.listen(3001);
 }
 bootstrap();
